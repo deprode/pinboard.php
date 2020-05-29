7df0268532b4e5262c2d8251f1e77aa9cad9b45d
deprode <sensitively+github@gmail.com>
2020-05-29 09:07:13 +0900

    Fix tests for client
    Add method for Mock creation

--- test/ClientTest.php
+++ test/ClientTest.php
@@ -6,12 +6,17 @@ use PinboardPHP\Lib\Client;
 
 class ClientTest extends TestCase
 {
-    public function testLastUpdatePosts()
+    public function getClientMock($response)
     {
         $client_mock = $this->createMock(\GuzzleHttp\Client::class);
-        $response_mock = new GuzzleHttp\Psr7\Response(200,[],'{"update_time":"2020-05-13T15:37:07Z"}');
+        $response_mock = new GuzzleHttp\Psr7\Response(200,[],$response);
         $client_mock->expects($this->any())->method('request')->willReturn($response_mock);
+        return $client_mock;
+    }
 
+    public function testLastUpdatePosts()
+    {
+        $client_mock = $this->getClientMock('{"update_time":"2020-05-13T15:37:07Z"}');
         $client = new Client(API_TOKEN, $client_mock);
         $response = $client->lastUpdatePosts();
         $this->assertEquals($response->getStatusCode(), 200);
@@ -20,117 +25,100 @@ class ClientTest extends TestCase
 
     public function testRecentPosts()
     {
-        $dummy = '{"date":"2020-05-20T00:54:47Z","user":"deprode","posts":[{"href":"https:\/\/example.com\/","description":"long description","extended":"","meta":"09876543210987654321098765432109","hash":"12345678901234567890123456789012","time":"2020-04-14T11:51:06Z","shared":"yes","toread":"yes","tags":"Testing"}]}';
-        $client_mock = $this->createMock(\GuzzleHttp\Client::class);
-        $response_mock = new GuzzleHttp\Psr7\Response(200,[],$dummy);
-        $client_mock->expects($this->any())->method('request')->willReturn($response_mock);
+        $response_body = '{"date":"2020-05-20T00:54:47Z","user":"deprode","posts":[{"href":"https:\/\/example.com\/","description":"long description","extended":"","meta":"09876543210987654321098765432109","hash":"12345678901234567890123456789012","time":"2020-04-14T11:51:06Z","shared":"yes","toread":"yes","tags":"Testing"}]}';
+        $client_mock = $this->getClientMock($response_body);
 
         $client = new Client(API_TOKEN, $client_mock);
         $response = $client->recentPosts(['tag' => 'Testing', 'count' => 100]);
         $this->assertEquals($response->getStatusCode(), 200);
-        $this->assertEquals($response->getBody()->getContents(), $dummy);
+        $this->assertEquals($response->getBody()->getContents(), $response_body);
     }
 
     public function testDatesPosts()
     {
-        $dummy = '{"user":"deprode","tag":"test","dates":{"2020-05-20":"1","2020-05-13":"4"}}';
-        $client_mock = $this->createMock(\GuzzleHttp\Client::class);
-        $response_mock = new GuzzleHttp\Psr7\Response(200,[],$dummy);
-        $client_mock->expects($this->any())->method('request')->willReturn($response_mock);
+        $response_body = '{"user":"deprode","tag":"test","dates":{"2020-05-20":"1","2020-05-13":"4"}}';
+        $client_mock = $this->getClientMock($response_body);
 
         $client = new Client(API_TOKEN, $client_mock);
         $response = $client->datesPosts(['tag' => 'Testing']);
         $this->assertEquals($response->getStatusCode(), 200);
-        $this->assertEquals($response->getBody()->getContents(), $dummy);
+        $this->assertEquals($response->getBody()->getContents(), $response_body);
     }
 
     public function testUserSecret()
     {
-        $dummy = '{"result":"6493a84f72d86e7de130"}';
-        $client_mock = $this->createMock(\GuzzleHttp\Client::class);
-        $response_mock = new GuzzleHttp\Psr7\Response(200,[],$dummy);
-        $client_mock->expects($this->any())->method('request')->willReturn($response_mock);
+        $response_body = '{"result":"6493a84f72d86e7de130"}';
+        $client_mock = $this->getClientMock($response_body);
+
         $client = new Client(API_TOKEN, $client_mock);
         $response = $client->userSecret();
         $this->assertEquals($response->getStatusCode(), 200);
-        $this->assertEquals($response->getBody()->getContents(), $dummy);
+        $this->assertEquals($response->getBody()->getContents(), $response_body);
     }
 
     public function testUserToken()
     {
-        $dummy = '{"result":"XOG86E7JIYMI"}';
-        $client_mock = $this->createMock(\GuzzleHttp\Client::class);
-        $response_mock = new GuzzleHttp\Psr7\Response(200,[],$dummy);
-        $client_mock->expects($this->any())->method('request')->willReturn($response_mock);
+        $response_body = '{"result":"XOG86E7JIYMI"}';
+        $client_mock = $this->getClientMock($response_body);
 
         $client = new Client(API_TOKEN, $client_mock);
         $response = $client->userToken();
         $this->assertEquals($response->getStatusCode(), 200);
-        $this->assertEquals($response->getBody()->getContents(), $dummy);
+        $this->assertEquals($response->getBody()->getContents(), $response_body);
     }
 
     public function testDeleteTag()
     {
-        $dummy = '{"result":"done"}';
-        $client_mock = $this->createMock(\GuzzleHttp\Client::class);
-        $response_mock = new GuzzleHttp\Psr7\Response(200,[],$dummy);
-        $client_mock->expects($this->any())->method('request')->willReturn($response_mock);
+        $response_body = '{"result":"done"}';
+        $client_mock = $this->getClientMock($response_body);
 
         $client = new Client(API_TOKEN, $client_mock);
         $response = $client->renameTag('foo', 'bar');
         $this->assertEquals($response->getStatusCode(), 200);
-        $this->assertEquals($response->getBody()->getContents(), $dummy);
+        $this->assertEquals($response->getBody()->getContents(), $response_body);
     }
 
     public function testRenameTag()
     {
-        $dummy = '{"result":"done"}';
-        $client_mock = $this->createMock(\GuzzleHttp\Client::class);
-        $response_mock = new GuzzleHttp\Psr7\Response(200,[],$dummy);
-        $client_mock->expects($this->any())->method('request')->willReturn($response_mock);
+        $response_body = '{"result":"done"}';
+        $client_mock = $this->getClientMock($response_body);
 
         $client = new Client(API_TOKEN, $client_mock);
         $response = $client->renameTag('foo', 'bar');
         $this->assertEquals($response->getStatusCode(), 200);
-        $this->assertEquals($response->getBody()->getContents(), $dummy);
+        $this->assertEquals($response->getBody()->getContents(), $response_body);
     }
 
     public function testGetTags()
     {
-        $dummy = '{"foo":"11","bar":"4"}';
-        $client_mock = $this->createMock(\GuzzleHttp\Client::class);
-        $response_mock = new GuzzleHttp\Psr7\Response(200,[],$dummy);
-        $client_mock->expects($this->any())->method('request')->willReturn($response_mock);
+        $response_body = '{"foo":"11","bar":"4"}';
+        $client_mock = $this->getClientMock($response_body);
 
         $client = new Client(API_TOKEN, $client_mock);
         $response = $client->getTags();
         $this->assertEquals($response->getStatusCode(), 200);
-        $this->assertEquals($response->getBody()->getContents(), $dummy);
+        $this->assertEquals($response->getBody()->getContents(), $response_body);
     }
 
     public function testNotesList()
     {
-        $dummy = '{"count":1,"notes":[{"id":"cf73bfc02e00edaa1e2b","hash":"0bbca3cba9246bbbda2c","title":"Paul Graham on Hirin\' The Ladies","length":"890","created_at":"2011-10-28 13:37:23","updated_at":"2011-10-28 13:37:23"}';
-        $client_mock = $this->createMock(\GuzzleHttp\Client::class);
-        $response_mock = new GuzzleHttp\Psr7\Response(200,[],$dummy);
-        $client_mock->expects($this->any())->method('request')->willReturn($response_mock);
+        $response_body = '{"count":1,"notes":[{"id":"cf73bfc02e00edaa1e2b","hash":"0bbca3cba9246bbbda2c","title":"Paul Graham on Hirin\' The Ladies","length":"890","created_at":"2011-10-28 13:37:23","updated_at":"2011-10-28 13:37:23"}';
+        $client_mock = $this->getClientMock($response_body);
 
         $client = new Client(API_TOKEN, $client_mock);
         $response = $client->notesList();
         $this->assertEquals($response->getStatusCode(), 200);
-        $this->assertEquals($response->getBody()->getContents(), $dummy);
+        $this->assertEquals($response->getBody()->getContents(), $response_body);
     }
 
     public function testNotesId()
     {
-        $dummy = '{"id":"cf73bfc02e00edaa1e2b","title":"Paul Graham on Hirin\' The Ladies","created_at":"2011-10-28 13:37:23","updated_at":"2011-10-28 13:37:23","length":556,"text":"[2] One advantage startups have over established companies is that there are no discrimination laws about starting businesses. For example, I would be reluctant to start a startup with a woman who had small children, or was likely to have them soon. ..., you can discriminate on any basis you want about who you start it with.","hash":"0bbca3cba9246bbbda2c"}';
-        $client_mock = $this->createMock(\GuzzleHttp\Client::class);
-        $response_mock = new GuzzleHttp\Psr7\Response(200,[],$dummy);
-        $client_mock->expects($this->any())->method('request')->willReturn($response_mock);
+        $response_body = '{"id":"cf73bfc02e00edaa1e2b","title":"Paul Graham on Hirin\' The Ladies","created_at":"2011-10-28 13:37:23","updated_at":"2011-10-28 13:37:23","length":556,"text":"[2] One advantage startups have over established companies is that there are no discrimination laws about starting businesses. For example, I would be reluctant to start a startup with a woman who had small children, or was likely to have them soon. ..., you can discriminate on any basis you want about who you start it with.","hash":"0bbca3cba9246bbbda2c"}';
+        $client_mock = $this->getClientMock($response_body);
 
         $client = new Client(API_TOKEN, $client_mock);
         $response = $client->noteById("cf73bfc02e00edaa1e2b");
         $this->assertEquals($response->getStatusCode(), 200);
-        $this->assertEquals($response->getBody()->getContents(), $dummy);
+        $this->assertEquals($response->getBody()->getContents(), $response_body);
     }
 }
